518bd4387fce8771e9166491b530aaf8bcbfb8d6
CorentinRZB <corentin.rznblm@gmail.com>
2021-12-01 00:11:19 +0100

    Sondage ! (sans chart)

--- frontend/src/pages/Sondages.js
+++ frontend/src/pages/Sondages.js
@@ -1,16 +1,77 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import Scroll from '../components/Scroll';
 import Logo from '../components/Logo';
 import Navigation from '../components/Navigation';
+import "../style/pages/_sondages.scss";
+import '../data.json';
 
 const Sondages = () => {
-    return (
-        <div className="widget">
-                <Navigation />
-                <Logo />
-                <Scroll left={'/structure'} right={'/contact'} />
-            </div>
-    );
-};
+
+    const [voteData, setVoteData] = useState();
+    const [totalVotes, setTotalVotes] = useState(0);
+    const [voted, setVoted] = useState(false);
+
+  const url = "http://localhost:3000/poll";
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setVoteData(data);
+        let sum = 0;
+        data.forEach(function (obj) {
+          sum += obj.votes;
+        });
+        setTotalVotes(sum);
+      });
+  }, []);
+
+  const submitVote = (e) => {
+    if(voted === false) {
+      const voteSelected = e.target.dataset.id;
+      const voteCurrent = voteData[voteSelected].votes;
+      voteData[voteSelected].votes = voteCurrent + 1;
+      setTotalVotes(totalVotes + 1);
+      setVoted(!voted);
+      const options = {
+        method: "POST",
+        body: JSON.stringify(voteData),
+        headers: { "Content-Type": "application/json" },
+      };
+      fetch(url, options)
+        .then((res) => res.json())
+        .then((res) => console.log(res));
+    }
+  };  
+  let pollOptions;
+  if (voteData) {
+    pollOptions = voteData.map((item) => {
+      return (
+        <li key={item.id}>
+          <button onClick={submitVote} data-id={item.id}>
+            {item.option}
+            <span>- {item.votes} Votes</span>
+          </button>          
+        </li>
+      );
+    });
+  }
+
+  return (
+    
+    <div className="widget">
+          <Logo />
+          <Navigation />
+          <Scroll left={'/structure'} right={'/contact'} />
+        <div className="poll">
+      <h1>Parmi ces Events, lesquels as-tu préféré ?</h1>
+      <ul className={voted ? "results" : "options"}>
+          {pollOptions}
+          </ul>
+      <p>Total Votes: {totalVotes}</p>
+    </div>
+    </div>
+  );
+  
+}
 
 export default Sondages;
