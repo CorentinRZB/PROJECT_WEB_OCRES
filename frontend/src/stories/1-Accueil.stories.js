b1c26025cc84f70ca638dda125c1c5e6b5cbe05f
CorentinRZB <corentin.rznblm@gmail.com>
2021-12-08 00:50:16 +0100

    Début Mongo

---
+++ frontend/src/stories/1-Accueil.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { linkTo } from '@storybook/addon-links';
+import { Welcome } from '@storybook/react/demo';
+import Formulaire from '../components/Formulaire';
+
+export default {
+  title: "Formulaire",
+  component: Formulaire,
+}
+
+export const Formulaire = () => <Formulaire/>
